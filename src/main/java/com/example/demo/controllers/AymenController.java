cc2e1d021df53078dd2afc97eefa030aa9163d53
Aymen <aymen@Aymens-MacBook-Pro.local>
2017-07-17 14:53:44 -0400

    editied AymenController for email sending (Not done)

--- src/main/java/com/example/demo/controllers/AymenController.java
+++ src/main/java/com/example/demo/controllers/AymenController.java
@@ -1,7 +1,41 @@
 package com.example.demo.controllers;
-import org.springframework.boot.SpringApplication;
 
+import java.io.UnsupportedEncodingException;
+import java.security.Principal;
+import javax.mail.internet.InternetAddress;
+import org.assertj.core.util.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.example.demo.models.User;
+import com.example.demo.repositories.UserRepository;
+import it.ozimov.springboot.mail.model.Email;
+import it.ozimov.springboot.mail.model.defaultimpl.DefaultEmail;
+import it.ozimov.springboot.mail.service.EmailService;
 
+@Controller
 public class AymenController {
+	
+	@Autowired
+	UserRepository userRepository;
 
+    @Autowired
+    public EmailService emailService;
+    public void sendEmailWithoutTemplating(String username, String email2) throws UnsupportedEncodingException {
+        final Email email = DefaultEmail.builder()
+                .from(new InternetAddress("aymehai@gmail.com", "Admin"))
+                .to(Lists.newArrayList(new InternetAddress(email2, username)))
+                .subject("goat")
+                .body("lel")
+                .encoding("UTF-8").build();
+        emailService.send(email);
+    }
+    
+    @PostMapping("/moreinfo")
+    public String creatememe(Model model, Principal principal) throws UnsupportedEncodingException {
+        User newUser = userRepository.findByUsername(principal.getName());
+        sendEmailWithoutTemplating(newUser.getFullName(), newUser.getEmail());
+        return "gallery";
+    }
 }
